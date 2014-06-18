665c54aa54e0e8847a951ed9ed63841fb1b1f010
Madk1030 <madk1030@gmail.com>
2014-06-18 13:45:23 -0400

    More files

--- intro_to_programming/6_Arrays/2.rb
+++ intro_to_programming/6_Arrays/2.rb
@@ -1,1 +1,4 @@
-puts "Array elements can only be set using their key values which are integers not strings"
+puts "First returns 1 and then the array is [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]
+] "
+
+puts "Second returns [1,2,3] and then the array is " [["b"], ["a", [1, 2, 3]]]
